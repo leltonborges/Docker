e34ec76b5ebb1ed974532ef7ec77978a6b1670d4
Lelton Borges <lelton.borges1@gmail.com>
2021-02-18 21:41:09 -0300

    Workers

--- email-worker-compose/app/app.sh
+++ email-worker-compose/app/app.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 
-pip install bottle==0.12.13
-python -u sender.py
+# pip install bottle==0.12.13 psycopg2==2.7.1
+# python -u sender.py
+
+pip install bottle==0.12.13 psycopg2==2.7.1
+python3 -u sender.py
